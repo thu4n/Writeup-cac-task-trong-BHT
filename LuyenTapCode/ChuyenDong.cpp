19a99850e2f911e3ab46632feddd0a99df510b75
Thuan Tong <53972592+TIw1nner@users.noreply.github.com>
2021-11-17 15:04:39 +0700

    Update ChuyenDong.cpp

--- LuyenTapCode/ChuyenDong.cpp
+++ LuyenTapCode/ChuyenDong.cpp
@@ -2,23 +2,18 @@
 
 using namespace std;
 
-int main()
-{
+int main(){
     int n, step = 0, counter = 0;
     cin >> n;
-    if(n <= 5)
-    {
+    if(n <= 5){
         cout << n;
         return 0;
     }
-    while(step < n)
-    {
+    while(step < n){
         step += 5;
         counter++;
-        if(n - step < 5)
-        {
-            if(n == step)
-            {
+        if(n - step < 5){
+            if(n == step){
                 cout << counter;
                 return 0;
             }
