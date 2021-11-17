a586105e3cee625128b669ef4f7e54860f27b503
Thuan Tong <53972592+TIw1nner@users.noreply.github.com>
2021-11-17 15:00:29 +0700

    Create ChuyenDong.cpp

---
+++ LuyenTapCode/ChuyenDong.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, step = 0, counter = 0;
+    cin >> n;
+    if(n <= 5)
+    {
+        cout << n;
+        return 0;
+    }
+    while(step < n)
+    {
+        step += 5;
+        counter++;
+        if(n - step < 5)
+        {
+            if(n == step)
+            {
+                cout << counter;
+                return 0;
+            }
+            step += n - step;
+            counter++;
+            cout << counter;
+            return 0;
+        }
+    }
+    return 0;
+}
